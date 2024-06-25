e239d7107e5d34638196737bd6cfc9f41b3df683
Aniketh Malyala <aniketh.malyala@gmail.com>
2024-06-25 03:10:06 -0700

    pleaes work reactnative app init

--- HandsomeDanTracker/server/src/utils/environment.ts
+++
@@ -1,3 +0,0 @@
-export const isCI = () => process.env.NODE_ENV === "ci";
-export const isDevelopment = () => process.env.NODE_ENV === "dev";
-export const isTest = () => process.env.NODE_ENV === "test";
